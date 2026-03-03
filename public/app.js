333fbf8a49a0795b2690781ee46bec37d6b5f994
ejuridicarodarltda-oss <ejuridica.rodar.ltda@gmail.com>
2026-03-03 13:44:27 -0300

    Delete public/app.js

--- public/app.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://kmhhndqeqvqhroftqrsc.supabase.co";
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttaGhuZHFlcXZxaHJvZnRxcnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMDk5MTEsImV4cCI6MjA4Nzc4NTkxMX0.Yif-C0SBqpde4Oguonb2Zq_yQTMYb33jyWqTJFuUxAo";
-
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
